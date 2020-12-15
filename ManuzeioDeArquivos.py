64c1f6ae50247cf46094325df6402264a74567b6
noe <caldas@email.com>
2020-12-15 11:08:28 -0400

    primeiros testes

---
+++ ManuzeioDeArquivos.py
@@ -0,0 +1,9 @@
+print("oi mundo")
+
+'''
+* ler o exif dos arquivos de uma pasta
+* filtrar por data
+* criar uma pasta de acordo com a data dos exif
+* mover arquivos para as pastas de acordo com as datas
+
+'''
